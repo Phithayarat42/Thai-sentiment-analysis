a86f09a42209faf939de0d0be71f81b35d2c435d
Phithayarat42 <76816871+Phithayarat42@users.noreply.github.com>
2022-03-31 21:24:28 +0700

    upload new files

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, render_template
+import re
+import string
+import pythainlp
+from pythainlp import word_tokenize, Tokenizer
+import pickle
+
+# Declare a Flask app
+app = Flask(__name__) 
+
+@app.route('/')
+def home():
+    return render_template('website.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    # If a form is submitted
+    if request.method == "POST":
+        
+        # Unpickle classifier
+        pipeline = pickle.load(open("pipeline.pkl", 'rb'))
+
+        # load the model from disk
+        customTokenizer = pickle.load(open("customTokenizer.pkl", 'rb'))
+        
+        newtext = request.form['text']
+
+        # Tokenize 
+        def LabelText(newText):
+            tokenizer = customTokenizer.word_tokenize(newText)
+
+            tokenizer = ','.join(map(str, tokenizer))
+
+            tokenizer = [re.sub(r',',' ',tokenizer)]
+
+            return tokenizer
+            
+        # Get prediction
+        my_predict = pipeline.predict(LabelText(newtext))
+
+    else:
+        my_predict = ""
+
+    return render_template("website.html", prediction = my_predict)
+
+# Running the app
+if (__name__)  == 'main':
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
+
+
